e58dacb2de6608b94cf99c33b901edfc45b159f1
Dary <Leontyev0302@mail.ru>
2024-10-16 22:09:52 +0500

    top-level statements added and Commands folder deleted

--- ScientificWork.Web/Program.cs
+++ ScientificWork.Web/Program.cs
@@ -1,35 +1,23 @@
-﻿// ReSharper disable UnassignedGetOnlyAutoProperty
+﻿using ScientificWork.Web;
+
+// ReSharper disable UnassignedGetOnlyAutoProperty
 // ReSharper disable UnusedMember.Global
 
-namespace ScientificWork.Web;
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
 
-/// <summary>
-/// Entry point class.
-/// </summary>
-internal sealed class Program
+startup.ConfigureServices(builder.Services, builder.Environment);
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+if (app == null)
 {
-    private static WebApplication? app;
-
-    /// <summary>
-    /// Entry point method.
-    /// </summary>
-    /// <param name="args">Program arguments.</param>
-    public static async Task<int> Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        var startup = new Startup(builder.Configuration);
-        startup.ConfigureServices(builder.Services, builder.Environment);
-        app = builder.Build();
-        startup.Configure(app, app.Environment);
-
-        if (app == null)
-        {
-            throw new InvalidOperationException("app is not initialized");
-        }
-
-        await app.InitAsync();
-        await app.RunAsync();
-
-        return 0;
-    }
+    throw new InvalidOperationException("app is not initialized");
 }
+
+await app.InitAsync();
+await app.RunAsync();
+
+return 0;
