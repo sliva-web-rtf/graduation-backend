509ff049a944bb98d3b31c1d179ab15cd35cdad6
flashikNet <133534020+flashikNet@users.noreply.github.com>
2024-11-22 04:18:29 +0500

    Write own utils and replace it in project

---
+++ ScientificWork.Infrastructure.Common/Utils/StringUtils.cs
@@ -0,0 +1,15 @@
+namespace ScientificWork.Infrastructure.Common.Utils;
+
+public static class StringUtils
+{
+    /// <summary>Joins the objects ignore empty ones.</summary>
+    /// <param name="separator">The string to use as a separator.</param>
+    /// <param name="values">The values.</param>
+    /// <returns>Concatenated string.</returns>
+    public static string JoinIgnoreEmpty(string separator, params string[] values)
+    {
+        ArgumentNullException.ThrowIfNull(separator);
+        ArgumentNullException.ThrowIfNull(values);
+        return string.Join(separator, values.Where(x => !string.IsNullOrWhiteSpace(x)));
+    }
+}
