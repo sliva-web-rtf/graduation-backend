da50ebf0995c8f05f7a71e374ddb5dc8ec6c5343
Dary <Leontyev0302@mail.ru>
2024-11-22 12:46:53 +0500

    update UpdateProfessorsScientificPortfolioCommand.cs

---
+++ ScientificWork.UseCases/Professors/UplaodProfessors/UploadProfessorsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace ScientificWork.UseCases.Professors.UplaodProfessors;
+
+public record UploadProfessorsCommand(IFormFile File) : IRequest;
